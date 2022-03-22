775bc0b17b4ab6a823f67dafb8e068651c22f6c7
hugosenari <hugosenari@gmail.com>
2022-03-22 01:07:29 -0300

    chore: re-index ../nimpkgs/b/beanstalkd

--- master/flake.nix
+++ master/flake.nix
@@ -8,8 +8,8 @@
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
   inputs.src-beanstalkd-master.flake = false;
+  inputs.src-beanstalkd-master.ref   = "refs/heads/master";
   inputs.src-beanstalkd-master.owner = "tormaroe";
-  inputs.src-beanstalkd-master.ref   = "master";
   inputs.src-beanstalkd-master.repo  = "beanstalkd.nim.git";
   inputs.src-beanstalkd-master.type  = "github";
   
